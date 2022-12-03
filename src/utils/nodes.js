e9d8354793171458307ef6718c11a1c72f926b80
Aracely Alba @AshkoreDev <aracelyalba8@gmail.com>
2022-12-03 20:46:03 +0100

    Added triangle structure

--- src/utils/nodes.js
+++ src/utils/nodes.js
@@ -7,4 +7,9 @@ const circleRadius = document.getElementById('circleRadius');
 const circleBtn = document.getElementById('circleBtn');
 const circleResult = document.getElementById('circleResult');
 
-// const circleBtn = document.getElementById('circleBtn');
+const triangleRightSide = document.getElementById('triangleRightSide');
+const triangleLeftSide = document.getElementById('triangleLeftSide');
+const triangleBase = document.getElementById('triangleBase');
+const triangleHeight = document.getElementById('triangleHeight');
+const triangleBtn = document.getElementById('triangleBtn');
+const triangleResult = document.getElementById('triangleResult');
