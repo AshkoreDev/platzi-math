7a332cb76bf8b4c921ed538f1d51b72c620170cf
Aracely Alba @AshkoreDev <aracelyalba8@gmail.com>
2023-01-02 21:39:36 +0100

    aaded median logic

--- src/utils/statistics.js
+++ src/utils/statistics.js
@@ -29,6 +29,37 @@ function arithmeticAverage(list) {
   return addition / list.length;
 }
 
+// MEDIAN
+let median;
+
+function pair(num) {
+
+  return (num % 2 === 0);
+}
+
+function medianC(list) {
+
+  const listBA = list.map((list) => Object.values(list));
+
+  const listTotalOrden = listBA.sort((elementA, elementB) => elementA - elementB);
+
+  const mediumList = parseInt(listTotalOrden.length / 2);
+
+  if(pair(list.length)) {
+
+    const element1 = list[mediumList - 1];
+    const element2 = list[mediumList];
+    const averageE1E2 = arithmeticAverage([element1, element2]);
+    median = averageE1E2;
+    return median;
+
+  } else {
+  	
+    median = list[mediumList];
+    return median;
+  }
+}
+
 if(addListBtn) {
   addListBtn.addEventListener('click', addData);
 }
