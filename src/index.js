90178f2bc8cf0c9e5ebf39372c3c17312775e2df
Aracely Alba @AshkoreDev <aracelyalba8@gmail.com>
2023-01-07 20:09:02 +0100

    Fixed body color

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './utils/nodes.js';
 import './components/navbar.js';
 import './components/footer.js';
 
-import './utils/geometry.js';
-import './utils/discounts.js';
-import './utils/statistics.js';
-import './utils/salaries.js';
+// import './utils/geometry.js';
+// import './utils/discounts.js';
+// import './utils/statistics.js';
+// import './utils/salaries.js';
